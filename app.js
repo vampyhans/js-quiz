3d7d16d8887fb0ac26b6910dafb3f4a296921669
unknown <hansahimasarawijesinghe@gmail.com>
2021-10-24 17:45:49 +0530

    alost done

---
+++ app.js
@@ -0,0 +1,28 @@
+const correctAnswers = ['A','A','A','B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    //check answers
+
+    userAnswers.forEach((answer,index) => {
+        if(answer === correctAnswers[index]){
+            score += 25;
+        }
+    });
+
+    //show result
+
+    scrollTo(0,0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
+    //window object (global object)
+
+});
+
